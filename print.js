81ad69c2c57cdd0dcecd2219417af2f438bd5006
Mark Busenitz <mbusenitz@mac232.att.net>
2014-09-12 22:47:38 -0700

    print.js

---
+++ print.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var bufferError = ''
+  , bufferOk = ''
+  , errRegex = new RegExp( ".*error:.*" )
+  , fileRegex = new RegExp( ".* (../native_booksmart/.*)" );
+
+function onOk(data) {
+	var matches;
+	bufferOk += data.toString();
+	matches = bufferOk.match( fileRegex );
+	if (matches) {
+		process.stdout.write( matches[1] + '\n' );
+		bufferOk = files.replace( fileRegex, '' );
+	}
+}
+
+function onError(data) {
+	var matches;
+	bufferError += data.toString();
+	matches = bufferError.match( errRegex );
+	if (matches) {
+		matches.forEach( function( match ) {
+			process.stdout.write( match + '\n' );
+		} );
+		bufferError = bufferError.replace( errRegex, '' );
+	}
+}
+
+module.exports.onOk = onOk;
+module.exports.onError = onError;
