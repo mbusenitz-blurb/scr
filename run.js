c4ce2bb24696bd81336060c72778c73dd39b5b37
Mark Busenitz <mbusenitz@mac232.att.net>
2014-09-22 22:03:35 -0700

    path crap

--- run.js
+++ run.js
@@ -24,10 +24,7 @@ function Configuration() {
 
   this.workingDir = path.dirname( this.defPath );
 
-  this.buildDir = path.resolve( 
-    instance.workingDir, 
-    path.join( '../', path.basename( instance.workingDir ) + '_build' ) 
-  ); 
+  this.buildDir = '/data/repositories/native_booksmart_build'; 
 
   this.sumFile = '.shasum';
 
