0804c86b876db70716da755c8c1fda547ed63a10
Mark Busenitz <mbusenitz@mac232.att.net>
2014-09-25 03:00:52 -0700

    run options

--- run.js
+++ run.js
@@ -9,8 +9,12 @@ var assert = require( 'assert' )
   , Qmaker = require( './qmaker' )
   , Maker = require( './maker' )
   , Runner = require( './runner' )
+  , Configuration = require( './configuration' )
   , program = require( 'commander' )
   , config = require( './config.json' ); 
+
+var controller = new events.EventEmitter()
+  , config = new Configuration(controller, config);
 
 program.version( '0.0.0' )
 	.option( '-t --test [match]', 'test matching cases' )
@@ -18,48 +22,12 @@ program.version( '0.0.0' )
   .option( '-m --make', 'only run make' )
   .parse( process.argv );
 
-function Configuration() {
-  var instance = this;
-
-  this.defPath = config.defPath,
-  this.workingDir = path.dirname( this.defPath );
-  this.target = config.target;
-  this.qmakeOptions = config.qmakeOptions;
-
-  if (config.hasOwnProperty('buildDir')) {
-    this.buildDir = config.buildDir; 
-  }
-  else {
-    this.buildDir = path.resolve( 
-      this.workingDir, 
-      path.join( '../', path.basename( this.workingDir ) + '_build' ) 
-    );
-  }
-
-  if (config.hasOwnProperty('sumFile')) {
-    this.sumFile = config.sumFile;
-  }
-  else {
-    this.sumFile = '.shasum'; 
-  }
-
-  if (config.hasOwnProperty('runOptions')) {
-    this.runOptions = config.runOptions;
-  }
-  else {
-    this.runOptions = [];
-  }
-}
-
-var config = new Configuration();
-
 if (program.test) {
 	config.test = program.test;
 }
 
 if (program.qmake) {
-  var controller = new events.EventEmitter()
-    , generator = new Generator( controller, config )
+  var generator = new Generator( controller, config )
     , consView = new Console( controller )
     , notifer = new Notifier( controller )
     , qmaker = new Qmaker( controller, config ); 
@@ -67,8 +35,7 @@ if (program.qmake) {
   controller.emit( 'check' );
 }
 else if (program.make) {
-  var controller = new events.EventEmitter()
-    , generator = new Generator( controller, config )
+  var generator = new Generator( controller, config )
     , consView = new Console( controller )
     , notifer = new Notifier( controller )
     , maker = new Maker( controller, config ); 
@@ -77,8 +44,7 @@ else if (program.make) {
 }
 else {
 
-  var controller = new events.EventEmitter()
-    , generator = new Generator( controller, config )
+  var generator = new Generator( controller, config )
     , consView = new Console( controller )
     , notifer = new Notifier( controller )
     , qmaker = new Qmaker( controller, config )
